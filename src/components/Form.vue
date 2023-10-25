e8133f6c5e563321ffd475fb41a09bce37c7ef34
Anna Panteleeva <apanteleevadev@gmail.com>
2023-10-25 13:58:13 +0300

    feat: add store and modal opening

---
+++ src/components/Form.vue
@@ -0,0 +1,55 @@
+<script lang="ts">
+export default {
+  name: 'FormWithSubmit',
+  data() {
+    return {
+      title: '',
+      text: '',
+    }
+  },
+  methods: {
+    hideForm() {
+      this.$store.commit("visibleForm", false);
+    }
+  },
+}
+</script>
+
+<template>
+  <div class="layout" v-if="this.$store.state.visibleForm">
+    <form class="form">
+      <div class="form__close" @click="hideForm">Х</div>
+      <h2>Создать заметочку</h2>
+      <input type="text" v-model="title">
+      <textarea name="text" v-memo="text"></textarea>
+      <button type="submit">Создать</button>
+    </form>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.layout {
+  min-width: 320px;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  background-color: rgba($color: #000000, $alpha: .6);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 4;
+  transition: all .6s linear;
+}
+
+.form {
+  display: flex;
+  flex-direction: column;
+  width: 282px;
+  min-height: 322px;
+  background: #FFFFFF;
+  border-radius: 10px;
+  z-index: 5;
+}
+</style>
---
+++ src/store/store.js
@@ -0,0 +1,191 @@
+import { createStore } from "vuex";
+
+export const store = createStore({
+	strict: true,
+	state() {
+		return {
+			userNotes: [],
+			favouriteNotes: [],
+			trashedNotes: [],
+			isNoteEmpty: true,
+			isNoteNew: true,
+			visibleForm: false,
+			noteId: "",
+			noteTitle: "",
+			noteText: "",
+			noteImages: [],
+			noteTheme: "dark",
+			noteFont: "glacial",
+			noteIsFavourite: false
+		}
+	},
+
+	mutations: {
+		visibleForm(state, payload) {
+			state.visibleForm = payload;
+		},
+
+		noteIsEmpty(state, payload) {
+			state.isNoteEmpty = payload;
+		},
+
+		noteIsNew(state, payload) {
+			state.isNoteNew = payload;
+		},
+		
+		setNoteTitle(state, payload) {
+			state.noteTitle = payload;
+		},
+
+		setNoteText(state, payload) {
+			state.noteText = payload;
+		},
+
+		addNoteImages(state, payload) {
+			state.noteImages.push(payload);
+		},
+
+		setNoteTheme(state, payload) {
+			state.noteTheme = payload;
+		},
+
+		setNoteFont(state, payload) {
+			state.noteFont = payload;
+		},
+
+		toggleFavourite(state) {
+			state.noteIsFavourite = !state.noteIsFavourite;
+		},
+
+		createOrTrash(state, payload) {
+			const newNote = {
+				id: new Date().toISOString(),
+				title: state.noteTitle.trim(),
+				text: state.noteText,
+				images: state.noteImages,
+				theme: state.noteTheme,
+				font: state.noteFont,
+				favourite: state.noteIsFavourite
+			}
+
+			if (payload === "create") {
+				state.userNotes.unshift(newNote);
+			} else if(payload === "trash"){
+				state.trashedNotes.unshift(newNote);
+			}
+		},
+
+		editNote(state, payload) {
+			state.noteId = payload.id;
+			state.noteTitle = payload.title;
+			state.noteText = payload.text;
+			state.noteImages = payload.images;
+			state.noteTheme = payload.theme;
+			state.noteFont = payload.font;
+			state.noteIsFavourite = payload.favourite;
+		},
+
+		// updateNote(state) {
+		// 	const noteToUpdate = findNote(state.userNotes, state.noteId);
+		// 	Object.assign(noteToUpdate, {
+		// 		title: state.noteTitle.trim(),
+		// 		text: state.noteText,
+		// 		images: state.noteImages,
+		// 		theme: state.noteTheme,
+		// 		font: state.noteFont,
+		// 		favourite: state.noteIsFavourite
+		// 	});
+		// },
+
+		resetNote(state) {
+			state.noteId = "";
+			state.noteTitle = "";
+			state.noteText = "";
+			state.noteImages = [];
+			state.noteTheme = "dark";
+			state.noteFont = "glacial";
+			state.noteIsFavourite = false;
+		},
+
+		// trashNote(state) {
+		// 	const noteToTrash = findNote(state.userNotes, state.noteId);
+		// 	state.trashedNotes.unshift(noteToTrash);
+		// 	state.userNotes = filterNotes(state.userNotes, noteToTrash.id);
+		// 	state.favouriteNotes = state.userNotes.filter(note => note.favourite);
+		// },
+
+		// restoreNote(state) {
+		// 	const noteToRestore = findNote(state.trashedNotes, state.noteId);
+		// 	state.userNotes.unshift(noteToRestore);
+		// 	state.trashedNotes = filterNotes(state.trashedNotes, noteToRestore.id);
+		// },
+
+		// removeEmptyNote(state) {
+		// 	state.userNotes = filterNotes(state.userNotes, state.noteId);
+		// },
+
+		// deleteNoteImages(state, payload) {
+		// 	state.noteImages.splice(payload, 1);
+		// },
+
+		// deleteNotes(state, payload) {
+		// 	if (payload === "one") {
+		// 		state.trashedNotes = filterNotes(state.trashedNotes, state.noteId);
+		// 	} else if (payload === "all") {
+		// 		state.trashedNotes = [];
+		// 	}
+		// },
+
+		addFavouriteNotes(state) {
+			state.favouriteNotes = state.userNotes.filter(note => note.favourite);
+		}
+	},
+
+	actions: {
+		moveToTrash({state, commit}) {
+			if (state.isNoteNew) {
+				commit("createOrTrash", "trash");
+				commit("resetNote");
+			} else {
+				commit("updateNote");
+				commit("trashNote");
+			}
+
+			commit("resetNote");
+			commit("noteDialogIsVisible", false);
+		},
+
+		restoreFromTrash({commit}) {
+			commit("restoreNote");
+			commit("resetNote");
+			commit("noteDialogIsVisible", false);
+			commit("addFavouriteNotes");
+		},
+
+		exitNote({state, commit}, page) {
+			if (page !== "trash") {
+				if (!state.isNoteEmpty) {
+					if (state.isNoteNew) {
+						commit("createOrTrash", "create");
+					} else {
+						commit("updateNote");
+					}
+					commit("addFavouriteNotes");
+				} else {
+					if (!state.isNoteNew) {
+						commit("removeEmptyNote");
+						commit("addFavouriteNotes");
+					}
+				}
+			}
+
+			commit("resetNote");
+			commit("noteDialogIsVisible", false);
+		},
+
+		emptyTrash({commit}, payload) {
+			commit("deleteNotes", payload);
+			commit("resetNote");
+		}
+	}
+})
